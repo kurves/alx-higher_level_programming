0eb78b618476072d34a1b3b131420419cb6a668d
kurves <kavssimply@gmail.com>
2024-01-02 12:00:00 -0800

    Function to print ASCII alpahbet

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,6 +6,8 @@ if number % 10 > 5:
             ".format(number, number % 10))
 elif number % 10 == 0:
     print("Last digit of {} is {} and is 0".format(number, number % 10))
+elif (number < 0) and abs((number % 10 < 0)):
+    print(f"Last digit of {number} is {-number % 10} is less than 6 and not 0")
 else:
     print("Last digit of {} is {} and is less than 6 and not 0\
             ".format(number, number % 10))
--- 0x01-python-if_else_loops_functions/README.md
+++ 0x01-python-if_else_loops_functions/README.md
@@ -1,3 +1,4 @@
 ## Python if-else loop conditions
 ### Function to chek if number is positive or negative
 ## Function to get the last digit of number
+### Program that prints the ASCII alphabet
