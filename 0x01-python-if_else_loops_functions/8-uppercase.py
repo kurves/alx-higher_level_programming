a4d32d7c103c9ef31ac9c4ab8835d5080ecec685
kurves <kavssimply@gmail.com>
2024-01-03 02:34:38 -0800

    Remove extra line

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,4 +4,4 @@ def uppercase(str):
         #if 'a' <= i <= 'z':
         stru = ord(i) - 32
         print("{}".format(chr(stru) if 'a' <= i <= 'z' else i), end="")
-    print("{}".format("\n"))
+uppercase("dghsdhj djhdj")        
