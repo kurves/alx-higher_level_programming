e6a8e69d9554ddae146379f3a5a09b3de362857e
kurves <kavssimply@gmail.com>
2024-01-03 02:41:04 -0800

    fix pycodestyle error

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str.lower():
-        #if 'a' <= i <= 'z':
         stru = ord(i) - 32
         print("{}".format(chr(stru) if 'a' <= i <= 'z' else i), end="")
     print()
-
