50e64f368ddb4e25fc1cecc5008cbe9e6e961b05
kurves <kavssimply@gmail.com>
2024-01-03 01:38:06 -0800

    fix: fix code to print desired output

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,6 @@ def uppercase(str):
     for i in str.lower():
         if 'a' <= i <= 'z':
             stru = ord(i) - 32
-            print(chr(stru), end="")
+            print("{}".format(chr(stru)), end="")
         else:
-            print(i, end="")
-    print()
+            print("{}".format(i), end="")
