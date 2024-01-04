b79207964ea9e102a536a90c02d1f0f5cac0921d
kurves <kavssimply@gmail.com>
2024-01-04 03:23:43 -0800

    correct the code for command line

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 import sys
-if len(sys.argv) == 1:
-    print("{} arguments".format(len(sys.argv) - 1))
-else:
-    if len(sys.argv) - 1 == 1:
-        print("{} argument:".format(len(sys.argv) - 1))
+def arguments():
+    if len(sys.argv) == 1:
+        print("{} arguments".format(len(sys.argv) - 1))
     else:
-        print("{} arguments:".format(len(sys.argv) - 1))
-        for i in range(1, len(sys.argv)):
-            print("{}: {}".format(i, sys.argv[i]))
+        if len(sys.argv) - 1 == 1:
+            print("{} argument:".format(len(sys.argv) - 1))
+        else:
+            print("{} arguments:".format(len(sys.argv) - 1))
+            for i in range(1, len(sys.argv)):
+                print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    arguments()
