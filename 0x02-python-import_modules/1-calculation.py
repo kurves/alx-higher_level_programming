c1a12a5afc47daf10944e21975b2d3f635726922
kurves <kavssimply@gmail.com>
2024-01-04 09:18:10 -0800

    fix pycosestyle error

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1  import add, div, mul, sub
+    from calculator_1 import add, div, mul, sub
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
