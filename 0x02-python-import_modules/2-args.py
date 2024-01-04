71c61e154cc4063345aac7d0f945f6aaef151eef
kurves <kavssimply@gmail.com>
2024-01-04 03:25:37 -0800

    fix pycodestyle error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import sys
+
+
 def arguments():
     if len(sys.argv) == 1:
         print("{} arguments".format(len(sys.argv) - 1))
@@ -10,5 +12,7 @@ def arguments():
             print("{} arguments:".format(len(sys.argv) - 1))
             for i in range(1, len(sys.argv)):
                 print("{}: {}".format(i, sys.argv[i]))
+
+
 if __name__ == "__main__":
     arguments()
