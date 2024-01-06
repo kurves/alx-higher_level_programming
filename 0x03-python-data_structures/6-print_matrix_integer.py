f1894c2dca323aa3e5f7bbf9e7dcff75a91e029f
kurves <kavssimply@gmail.com>
2024-01-06 00:55:43 -0800

    function to print maatrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i range(len(row)):
+        for i in range(len(row)):
             if i == len(row) - 1:
-                print("{:d} ".format(row[i]))
+                print("{:d}".format(row[i]))
             else:
-                print("{:d}".format(row[i]), end="")
+                print("{:d} ".format(row[i]), end="")
