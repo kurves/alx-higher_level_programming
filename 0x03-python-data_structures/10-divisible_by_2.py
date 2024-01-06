459226f194a049a9a7cc961180741f96c1c580d5
kurves <kavssimply@gmail.com>
2024-01-06 01:04:24 -0800

    function to ceck doubles of two

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    doubled = []
-    for num in my_list:
-        if num % 2 == 0:
-            doubled.append(True)
-    else:
-        doubled.append(False)
+    doubled = [num % 2 == 0 for num in my_list]
     return doubled
