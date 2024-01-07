f23ad9a978580fc25409d25b8f9dd4f378eced59
kurves <kavssimply@gmail.com>
2024-01-07 03:43:40 -0800

    Fix pycodestyle error

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import dis
+
+
 def magic_calculation(a, b):
     c = 0
     if a < b:
         from magic_calculation_102 import add, sub
-        c = add( a, b)
+        c = add(a, b)
         for i in range(4, 7):
             c = add(c, i)
     else:
