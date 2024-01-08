1fb2f74ac3aa31f9ed0c3eca3f41247d72fa4447
kurves <kavssimply@gmail.com>
2024-01-07 23:18:38 -0800

    function to remove c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_string = ""
     for character in my_string:
-        if character not in 'cC':
-            new_string += c
+        if character.lower() != 'c':
+            new_string += character
     return new_string
