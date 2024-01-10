0c8cb96978cb4d9fa3c45e758ed5271d9b0d04b8
kurves <kavssimply@gmail.com>
2024-01-10 09:52:39 -0800

    fix pycodestyle error

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    Values = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+    Values = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     result = 0
     prev_value = 0
     if type(roman_string) != str:
