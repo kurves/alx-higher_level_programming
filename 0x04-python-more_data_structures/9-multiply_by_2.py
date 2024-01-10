9a2fa3d057247117ce188425301ae2f1cea30ca2
kurves <kavssimply@gmail.com>
2024-01-10 07:44:54 -0800

    function to double dictionaty values

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    doubled _dictionary = a_dictionary.copy()
-    for k,v in doubled_dictionary:
-        v * 2
+    doubled_dictionary = a_dictionary.copy()
+    for k,v in doubled_dictionary.items():
+        doubled_dictionary[k] = v * 2
     return doubled_dictionary
