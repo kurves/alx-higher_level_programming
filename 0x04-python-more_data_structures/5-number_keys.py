9dfc83060b508773d5c3086fb65433327bd09366
kurves <kavssimply@gmail.com>
2024-01-10 02:37:15 -0800

    function to get nuber of keys

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    for k in a_dictionary:
-        length = k.length
-    return k.length
+    length = len(a_dictionary.keys())
+    return length
