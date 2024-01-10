0f58cecbe0f65afde7cf0a2d812deb695e8d41e4
kurves <kavssimply@gmail.com>
2024-01-10 09:45:55 -0800

    function to update dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,7 +2,7 @@
 def update_dictionary(a_dictionary, key, value):
     if key in a_dictionary:
         a_dictionary[key] = value
-      else:
+    else:
         a_dictionary[key] = value
 
     return a_dictionary
