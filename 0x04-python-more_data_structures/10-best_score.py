b29e6ea110d8658081dc86b3258382ad8145b92f
kurves <kavssimply@gmail.com>
2024-01-10 08:02:53 -0800

    function to get the maximum value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    for key, value in doubled_dictionary.items():
-        if not v:
-            return none
-        
+    if not a_dictionary:
+        return None
+    else:
+        max_value = max(a_dictionary, key = a_dictionary.get)
+        return max_value
