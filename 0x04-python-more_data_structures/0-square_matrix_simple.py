ef89b6fc076cfbdb03f4fb1716c033372a2e5778
kurves <kavssimply@gmail.com>
2024-01-10 09:35:37 -0800

    funtion to double matrix integers

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,4 +4,4 @@ def square_matrix_simple(matrix=[]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             squared_matrix[i][j] = matrix[i][j] ** 2
-    return(squared_matrix)
+    return squared_matrix
