6675f671d682d19b45998c59112158917fb8a0d9
kurves <kavssimply@gmail.com>
2024-01-11 23:37:03 -0800

    fix logic error

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     key_delete = [key for key, val in a_dictionary.items() if val == value]
-    for key in key_delete:
+    for key in reversed(key_delete):
         del a_dictionary[key]
+    return a_dictionary
