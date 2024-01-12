a636bdc1af6dbaf59e537d7e2bffe4164703453c
kurves <kavssimply@gmail.com>
2024-01-11 22:45:32 -0800

    Function to convert roman value to inteer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,8 @@ def roman_to_int(roman_string):
     Values = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     result = 0
     prev_value = 0
-    if type(roman_string) != str:
-        return None
+    if type(roman_string) != str || None:
+        return 0
     for i in reversed(roman_string):
         value = Values[i]
 
