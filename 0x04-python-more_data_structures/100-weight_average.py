d69cd9425197776ad22fa6b99414930c5aecc1f4
kurves <kavssimply@gmail.com>
2024-01-11 23:14:45 -0800

    Function to get weighted avreage

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,14 +2,12 @@
 def weight_average(my_list=[]):
     if not my_list:
         return 0
-    else:
-        score = 0
-        weight = 0
-        for s, w in my_list:
-            score += s * w
-            weight += w
-            if total_weight != 0:
-                weighted_average = total_score / total_weight
-                return weighted_average
-            else:
-                return 0
+    score = 0
+    weight = 0
+    for s, w in my_list:
+        score += s * w
+        weight += w
+        if weight == 0:
+            return 0
+        weighted_average = score / weight
+        return weighted_average
