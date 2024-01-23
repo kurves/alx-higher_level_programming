93239eae114ae6883acd6b49fcf4fd6e7c59e1de
kurves <kavssimply@gmail.com>
2024-01-23 09:03:59 -0800

    fcuntion to print area

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+class Square:
+    """class defining a class"""
+    def __init__(self, size=0):
+        """
+        initializes a new instance of class
+        Args:
+            size(int): optoonal
+        Raises:
+            TypeError: if not int
+            ValueError: if less than 0
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Get the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size():
+        """
+        Set the size.
+
+        Args:
+            value (int): The size of each side.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    def area(self):
+        """
+        calculates and return area
+        Returns: int
+        """
+        return self.__size ** 2
+    def print(self):
+        """prints the area"""
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
