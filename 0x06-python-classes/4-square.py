48a00351baadeb04526118fba188a78d19c67652
kurves <kavssimply@gmail.com>
2024-01-23 10:49:14 -0800

    fix compilation error

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -32,7 +32,7 @@ class Square:
             TypeError: If value is not an integer.
             ValueError: If value is less than 0.
         """
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
