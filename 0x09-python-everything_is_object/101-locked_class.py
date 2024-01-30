a4a69d9a934c252a9d6aa599c445f5c428cfcd4b
kurves <kavssimply@gmail.com>
2024-01-30 08:11:26 -0800

    function to create a locked class

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""define a class"""
+
+class LockedClass:
+    """
+    class that defines a locked class
+    """
+    __slots__ = ["first_name"]
+    def __init__(self):
+        """
+        empty function
+        """
+        pass
+
+
+
