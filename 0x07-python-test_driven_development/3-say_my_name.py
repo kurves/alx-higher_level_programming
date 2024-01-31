afdd63e70b4eacb1d34d7aeef854d5b7d1fa6387
kurves <kavssimply@gmail.com>
2024-01-31 00:07:41 -0800

    fix pycodestyle style

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -6,11 +6,13 @@ Parameters: first name and last name
 raises: typerror
 """
 
+
 def say_my_name(first_name, last_name=""):
     """
     function that prints first and last name
     """
     if not isinstance(first_name, str) or not isinstance(last_name, str):
-        raise TypeError("first_name must be a string or last_name must be a string")
+        raise TypeError("first_name must be \
+            a string or last_name must be a string")
 
     print("My name is {} {}".format(first_name, last_name))
