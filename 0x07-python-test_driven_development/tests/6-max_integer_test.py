7f194c3692d69434820d81a27482efa586d2a745
kurves <kavssimply@gmail.com>
2024-01-31 00:30:19 -0800

    function to write unittest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -31,13 +31,20 @@ class TestMaxInteger(unittest.TestCase):
         """
         Tests finding the maximum in an empty list.
         """
-        self.assertEqual(max_integer([]), None)
+        self.assertIsNone(max_integer([]))
 
     def test_single_element_list(self):
         """Tests finding the maximum in a list with a single element.
         """
         self.assertEqual(max_integer([9]), 9)
         self.assertEqual(max_integer([-4]), -4)
+
+    def test_floats(self):
+        self.assertEqual(max_integer([6.1, 2.2, 4.3]), 6.3)
+        self.assertEqual(max_integer([4.3, 2.2, 6.1]), 6.3)
+
+    def test_strings(self):
+        self.assertEqual(max_integer(['a', 'b', 'c']), 'c')
 
     def test_non_integer_values(self):
         """
