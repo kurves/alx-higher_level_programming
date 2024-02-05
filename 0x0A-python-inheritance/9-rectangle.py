81941ecf95b44ed5f7b42cb7994b7a9e45bfa9f3
kurves <kavssimply@gmail.com>
2024-02-05 10:21:05 -0800

    add documentation

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,32 +1,16 @@
 #!/usr/bin/python3
 """base class base geometry"""
 
+
 class BaseGeometry:
-    """
-    BaseGeometry class with area and integer_validator methods.
-    """
+    """BaseGeometry class with area and integer_validator methods."""
 
     def area(self):
-        """
-        Public instance method to calculate the area.
-
-        Raises:
-            Exception: Always raises an exception with the message 'area() is not implemented'.
-        """
+        """Public instance method to calculate the area"""
         raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
-        """
-        Public instance method to validate an integer value.
-
-        Parameters:
-            name (str): The name of the value being validated.
-            value (int): The value to be validated.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than or equal to 0.
-        """
+        """Public instance method to validate an integer value."""
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
@@ -34,37 +18,19 @@ class BaseGeometry:
 
 
 class Rectangle(BaseGeometry):
-    """
-    Rectangle class inherits from BaseGeometry.
-    """
+    """Rectangle class inherits from BaseGeometry."""
 
     def __init__(self, width, height):
-        """
-        Initializes a Rectangle instance with given width and height.
-
-        Parameters:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-        """
+        """Initializes a Rectangle instance with given width and height."""
         self.__width = width
         self.__height = height
         self.integer_validator("width", width)
         self.integer_validator("height", height)
 
-     def area(self):
-        """
-        Public instance method to calculate the area of the rectangle.
-
-        Returns:
-            int: The area of the rectangle (width * height).
-        """
+    def area(self):
+        """Public instance method to calculate the area of the rectangle."""
         return self.__width * self.__height
 
     def __str__(self):
-        """
-        Returns the rectangle description as a string.
-
-        Returns:
-            str: The rectangle description in the format '[Rectangle] <width>/<height>'.
-        """
+        """Returns the rectangle description as a string."""
         return f"[Rectangle] {self.__width}/{self.__height}"
