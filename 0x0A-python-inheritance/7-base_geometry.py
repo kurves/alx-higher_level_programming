abb860d026375fddc10ed002b5aaad3c32e1e637
kurves <kavssimply@gmail.com>
2024-02-05 10:13:46 -0800

    add documentation

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,29 +1,15 @@
 #!/usr/bin/python3
-
 """Define a class base geometry ande ares"""
 
 
 class BaseGeometry:
     """class defining a class"""
     def area(self):
-        """
-        Public instance method to calculate the area.
-
-        Raises:
-            Exception: Always raises an exception with the message 'area() is not implemented'.
-        """
+        """Public instance method to calculate the area."""
         raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
-
-        """
-        initializes a new instance of class
-        Args:
-            size(int): optoonal
-        Raises:
-            TypeError: if not int
-            ValueError: if less than 0
-        """
+        """initializes a new instance of class"""
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
