789c537700d2a952734d9b45a3cd8fa745885198
kurves <kavssimply@gmail.com>
2024-02-05 08:24:04 -0800

    function to instantiate area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Define a class that instanitiate a new instance"""
+
+
+class Square:
+    """classs tat defines a square."""
+    def area(self):
+        """"
+        initializes a new instance
+
+        Args:
+            size(int)
+        """
+        raise Exception('area() is not implemented')
