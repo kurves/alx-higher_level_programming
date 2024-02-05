947650eff74cbf0fcb1e8fc5606d7c90ede6023c
kurves <kavssimply@gmail.com>
2024-02-05 09:51:46 -0800

    add function documentation

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
+"""Defines an object attribute lookup function."""
 
 def lookup(obj):
-    """
-    function to return available object
-    """
+    """function to return available object"""
     return(dir(obj))
