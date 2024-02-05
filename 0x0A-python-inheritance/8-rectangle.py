9a545808f3be602c1a1bda9e2d51cc08fa993ee3
kurves <kavssimply@gmail.com>
2024-02-05 10:16:43 -0800

    fix pycodestyle error

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,51 +1,26 @@
 #!/usr/bin/python3
 """classs Base Geometry"""
 
+
 class BaseGeometry:
-    """
-    BaseGeometry class with area
-    """
+    """BaseGeometry class with area"""
 
     def area(self):
-        """
-        Public instance method to calculate the area.
-
-        Raises:
-            Exception:  message 'area() is not implemented'.
-        """
+        """Public instance method to calculate the area."""
         raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
-        """
-        Public instance method to validate an integer value.
-
-        Parameters:
-            name (str): The name of the value being validated.
-            value (int): The value to be validated.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than or equal to 0.
-        """
+        """Public instance method to validate an integer value."""
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
 
-
 class Rectangle(BaseGeometry):
-    """
-    Rectangle class inherits from BaseGeometry.
-    """
+    """Rectangle class inherits from BaseGeometry."""
 
     def __init__(self, width, height):
-        """
-        Initializes a Rectangle instance with given width and height.
-
-        Parameters:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-        """
+        """Initializes a Rectangle instance with given width and height."""
         self.__width = width
         self.__height = height
         self.integer_validator("width", width)
