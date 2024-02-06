0de10ce388437bc2ef0053433635d7f80ff51b48
kurves <kavssimply@gmail.com>
2024-02-06 18:04:35 +0000

    function that writes to file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 """function that reads from stdin"""
 
+
 def write_file(filename="", text=""):
-    """function to read file"""
-    with open(filename, 'w') as f:
-        data = f.write(text)
-    return data
+	"""function to read file"""
+	try:
+		with open(filename, 'w', encoding="utf-8") as f:
+	        	data = f.write(text)
+		return data
+	except:
+		return 0
--- 0x0B-python-input_output/README.md
+++ 0x0B-python-input_output/README.md
@@ -1,2 +1,3 @@
 ## 0x0B. Python - Input/Output
 ### function that reads a text file (UTF8) and prints it to stdou
+###  function that writes a string to a text file (UTF8) and returns the number of characters written
