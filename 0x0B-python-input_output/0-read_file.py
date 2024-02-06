fcc7a82583afb1be07bbbeb622586c67a8fbd52e
kurves <kavssimply@gmail.com>
2024-02-06 17:49:36 +0000

    function to readd from stdin

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,7 +2,7 @@
 """function that reads from stdin"""
 
 def read_file(filename=""):
-    """function to read file"""
-    with open(filename, 'r') as f:
-        sentences = f.read()
-        print(sentences)
+	"""function to read file"""
+	with open(filename, 'r', encoding="utf-8") as f:
+        	sentences = f.read()
+	        print(sentences, end="")
--- 0x0B-python-input_output/README.md
+++ 0x0B-python-input_output/README.md
@@ -1,1 +1,2 @@
 ## 0x0B. Python - Input/Output
+### function that reads a text file (UTF8) and prints it to stdou
