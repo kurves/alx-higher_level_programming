37635044ce967058fd6c1999f9144b694c122ba3
kurves <kavssimply@gmail.com>
2024-02-07 08:43:34 +0000

    class definiton of student

--- 0x0B-python-input_output/10-main.py
+++ 0x0B-python-input_output/10-main.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-"""class student"""
+student = __import__('10-student').Student
 
+student_1 = Student("John", "Doe", 23)
+student_2 = Student("Bob", "Dylan", 27)
 
-class Student:
-    def __init__(self, first_name, last_name, age):
-        """Initialize a Student instance with first name, last name, and age."""
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
+j_student_1 = student_1.to_json()
+j_student_2 = student_2.to_json(['first_name', 'age'])
+j_student_3 = student_2.to_json(['middle_name', 'age'])
 
-    def to_json(self, attrs=None):
-        """Retrieve a dictionary representation of a Student instance."""
-        if attrs is None:
-            return self.__dict__
-        else:
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+print(j_student_1)
+print(j_student_2)
+print(j_student_3)
---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""class student"""
+
+
+class Student:
+    """class defintion"""
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance with first name, last name, and age."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve a dictionary representation of a Student instance."""
+        if attrs is None:
+            return {
+                "first_name": self.first_name,
+                "last_name": self.last_name,
+                "age": self.age,
+            }
+        else:
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
