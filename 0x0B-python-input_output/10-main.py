6315e846526331de1e85072804aea103c88532dd
kurves <kavssimply@gmail.com>
2024-02-07 08:33:42 +0000

    class definition of syudent

---
+++ 0x0B-python-input_output/10-main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""class student"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance with first name, last name, and age."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve a dictionary representation of a Student instance."""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
--- 0x0B-python-input_output/README.md
+++ 0x0B-python-input_output/README.md
@@ -7,3 +7,5 @@
 ### function that writes an Object to a text file, using a JSON representation:
 ### function that creates an Object from JSON filea 
 ###  script that adds all arguments to a Python list, and then save them to a file
+### function that returns the dictionary description with simple data structure (list, dictionary, string,
+###  class Student that defines a student by
