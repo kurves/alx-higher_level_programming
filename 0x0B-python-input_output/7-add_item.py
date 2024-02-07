b21a64b507905ac5804bb8fef7c166363af0050c
kurves <kavssimply@gmail.com>
2024-02-07 08:10:16 +0000

    function to add all arguments to file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,18 +4,17 @@
 import json
 import sys
 from os.path import exists
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
 
-
-def add_items_and_save(arguments):    
-    """function to load json"""
-    if exists("add_item.json"):
-        items = load_from_json_file("add_item.json")
-    else:
-        items = []
-    items.extend(arguments)
-    save_to_json_file(items, "add_item.json")
 if __name__ == "__main__":
-    arguments = sys.argv[1:]
-    add_items_and_save(arguments)
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+    def add_items_and_save(arguments):    
+        """function to load json"""
+        if exists("add_item.json"):
+            items = load_from_json_file("add_item.json")
+        else:
+            items = []
+        items.extend(arguments)
+        save_to_json_file(items, "add_item.json")
