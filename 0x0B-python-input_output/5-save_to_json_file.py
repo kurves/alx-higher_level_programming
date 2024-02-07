1f2fb1613a4cd85a5e8d992797da19cbcb6a663f
kurves <kavssimply@gmail.com>
2024-02-07 07:43:56 +0000

    fix pycodestyle

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,7 +5,7 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-	"""save to file function"""
-	with open(filename, "w") as file:
-		data = json.dump(my_obj, file)
-	return data
+    """save to file function"""
+    with open(filename, "w") as file:
+        data = json.dump(my_obj, file)
+    return data
