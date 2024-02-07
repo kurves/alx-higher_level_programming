f0ceebde8802bc043e6b7bf20c6eb124cec251a7
kurves <kavssimply@gmail.com>
2024-02-07 07:35:51 +0000

    function to append text to file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """function that appends from stdin"""
 
+
 def append_write(filename="", text=""):
     """function to read file"""
-    try:
-        with open(filename, 'a') as f:
-            data = f.write(text)
-            return data
-    except:
-        return 0
+    with open(filename, 'a') as f:
+        data = f.write(text)
+    return data
