f4cda4aa925f017c482ae1e9f4fd411204b547f3
kurves <kavssimply@gmail.com>
2024-02-07 07:25:28 +0000

    make file executable

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,10 +3,10 @@
 
 
 def write_file(filename="", text=""):
-	"""function to read file"""
-	try:
-		with open(filename, 'w', encoding="utf-8") as f:
-			data = f.write(text)
-		return data
-	except:
-		return 0
+    """function to read file"""
+    try:
+        with open(filename, 'w', encoding="utf-8") as f:
+            data = f.write(text)
+        return data
+    except:
+        return 0
