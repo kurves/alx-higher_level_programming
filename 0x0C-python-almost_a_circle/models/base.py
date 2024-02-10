228b84fbcf91119ec726adc332530efaf51a2b84
kurves <kavssimply@gmail.com>
2024-02-10 19:07:35 +0000

    function to return json representation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,3 +23,12 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the JSON string"""
+        if list_objs is None:
+            list_objs = []
+        filename = cls.__name__ + ".json"
+        with open(filename, mode='w') as file:
+            file.write(cls.to_json_string([obj.to_dictionary() for obj in list_objs]))
