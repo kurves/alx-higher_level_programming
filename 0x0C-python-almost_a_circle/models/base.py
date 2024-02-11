a91c418d3554b6328ead1a06f814918edd32cd69
kurves <kavssimply@gmail.com>
2024-02-11 07:14:37 +0000

    functiont to convert to json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -32,3 +32,11 @@ class Base:
         filename = cls.__name__ + ".json"
         with open(filename, mode='w') as file:
             file.write(cls.to_json_string([obj.to_dictionary() for obj in list_objs]))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the list of the JSON"""
+        if not json_string:
+            return []
+        else:
+            return json.loads(json_string)
