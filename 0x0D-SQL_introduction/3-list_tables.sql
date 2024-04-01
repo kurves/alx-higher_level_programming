ae0b793b523cb944ec3893404dfa74e03274a8a1
kurves <kavssimply@gmail.com>
2024-04-01 09:48:49 -0700

    script that lists all the tables of a database in your MySQL server

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# Check if the database name is provided as argument
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <database_name>"
     exit 1
 fi
 
-# Store the database name passed as argument
 database_name="$1"
+
+
+read -p "Enter MySQL username: " username
+read -sp "Enter MySQL password: " password
+echo
+
+mysql -u "$username" -p"$password" -e "USE $database_name; SHOW TABLES;"
