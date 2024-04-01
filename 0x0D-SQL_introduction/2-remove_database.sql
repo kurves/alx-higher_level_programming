ab38f4d3aff64dab3fa6913772af9d173c97a1f3
kurves <kavssimply@gmail.com>
2024-04-01 09:40:27 -0700

    script that deletes the database hbtn_0c_0

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Prompt the user for MySQL credentials
+-- script to delete database
 read -p "Enter MySQL username: " username
 read -sp "Enter MySQL password: " password
 
-# Use the mysql command to connect to the server and delete the database
 mysql -u "$username" -p"$password" -e "DROP DATABASE IF EXISTS hbtn_0c_0;"
