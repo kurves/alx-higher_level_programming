16b4665c4cb9a4e6995aa895ca509e12899aab68
kurves <kavssimply@gmail.com>
2024-04-03 03:24:46 -0700

    script that creates a table called first_table in the current database

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -8,3 +8,12 @@ fi
 
 # Extract the database name from the command-line argument
 database_name="$1"
+
+
+sql_query="CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);"
+
+# Run the SQL query to create the table
+mysql -u your_username -p your_password -e "USE $database_name; $sql_query"
