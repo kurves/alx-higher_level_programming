b65236eb8e0ee434c85e243ecb98f93ace45de1d
kurves <kavssimply@gmail.com>
2024-04-03 03:28:35 -0700

    script to list databases in currrent server

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Check if the correct number of arguments is provided
+-- script to list databases
+
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <database_name>"
     exit 1
 fi
 
-# Extract the database name from the command-line argument
 database_name="$1"
 
 
@@ -15,5 +15,4 @@ sql_query="CREATE TABLE IF NOT EXISTS first_table (
     name VARCHAR(256)
 );"
 
-# Run the SQL query to create the table
 mysql -u your_username -p your_password -e "USE $database_name; $sql_query"
