f14c5259f42087f4d42191b4fcc1badc85a205d5
kurves <kavssimply@gmail.com>
2024-04-05 08:47:22 -0700

    script that inserts a new row in the table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Usage: ./script_name database_name
-
+-- script to insert row
+-- insert name and id
 database_name=$1
 
 mysql -u root -p -e "USE $database_name; INSERT INTO first_table (id, name) VALUES (89, 'Best School');"
