f51b0c11ce4978848215332ee698c17c36f9ca79
kurves <kavssimply@gmail.com>
2024-04-07 09:58:42 -0700

    script that updates the score of Bob

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-# Check if the correct number of arguments is provided
+--script to records list
+
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <database_name>"
     exit 1
 fi
 
-# Extract the database name from the command-line argument
 DB_NAME="$1"
 
-# Define the SQL query to update the score of Bob to 10
 SQL_QUERY="UPDATE ${DB_NAME}.second_table SET score = 10 WHERE name = 'Bob';"
 
-# Execute the SQL query using the mysql command
 mysql -e "$SQL_QUERY"
