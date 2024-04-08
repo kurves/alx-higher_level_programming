ffcf6706ed3e7312fb14d6786962dacfd7780364
kurves <kavssimply@gmail.com>
2024-04-08 13:46:55 -0700

    fix semistandard codingstyle

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const arg = process.argv[2];
 
 if (!isNaN(arg) && Number.isInteger(parseInt(arg))) {
-    console.log(`My number: ${parseInt(arg)}`);
+  console.log(`My number: ${parseInt(arg)}`);
 } else {
-    console.log("Not a number");
+  console.log('Not a number');
 }
