955983b2ca60ebc38555c44c418eae4de4111789
kurves <kavssimply@gmail.com>
2024-04-08 13:38:23 -0700

    fix semistandard coding style

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,9 +2,9 @@
 const [arg1, arg2] = process.argv.slice(2);
 
 if (arg1 && arg2) {
-  console.log(arg1 +  'is' + arg2);
-} else if(arg1) {
-  console.log(arg1 + 'is undefine');
-}else {
+  console.log(arg1 + ' is ' + arg2);
+} else if (arg1) {
+  console.log(arg1 + ' is undefined');
+} else {
   console.log('undefined is undefined');
 }
