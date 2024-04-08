54eef4efbb448cd23062cff10c2a80ab20cb3274
kurves <kavssimply@gmail.com>
2024-04-08 14:47:28 -0700

    fix semistanadard coding style

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,19 +3,17 @@
 const args = process.argv.slice(2).map(Number);
 
 if (args.length <= 1) {
-    console.log(0);
+  console.log(0);
 } else {
-    let max = -Infinity;
-    let secondMax = -Infinity;
-
-    for (let i = 0; i < args.length; i++) {
-        if (args[i] > max) {
-            secondMax = max;
-            max = args[i];
-        } else if (args[i] > secondMax && args[i] !== max) {
-            secondMax = args[i];
-        }
+  let max = -Infinity;
+  let secondMax = -Infinity;
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] > max) {
+      secondMax = max;
+      max = args[i];
+    } else if (args[i] > secondMax && args[i] !== max) {
+      secondMax = args[i];
     }
-
-    console.log(secondMax);
+  }
+  console.log(secondMax);
 }
