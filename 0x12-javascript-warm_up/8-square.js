2886673f80b5d5cc939b53680540f28ae1ad7896
kurves <kavssimply@gmail.com>
2024-04-08 14:18:04 -0700

    fix semi standard coidng style

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,18 +2,18 @@
 const arg = process.argv[2];
 
 if (!isNaN(arg) && Number.isInteger(parseInt(arg))) {
-    const size = parseInt(arg);
-    if (size <= 0) {
-        console.log('Size must be a positive integer');
-    } else {
-        for (let i = 0; i < size; i++) {
-            let row = '';
-            for (let j = 0; j < size; j++) {
-                row += 'X';
-            }
-            console.log(row);
-        }
+  const size = parseInt(arg);
+  if (size <= 0) {
+    console.log('Size must be a positive integer');
+  } else {
+    for (let i = 0; i < size; i++) {
+      let row = '';
+      for (let j = 0; j < size; j++) {
+        row += 'X';
+      }
+      console.log(row);
     }
+  }
 } else {
-    console.log('Missing size');
+  console.log('Missing size');
 }
