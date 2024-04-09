186c2da919bd16f9a451991bcd84c0e7c6355e2d
kurves <kavssimply@gmail.com>
2024-04-09 14:12:54 -0700

    fix semistandard codingstyle

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-exports.callMeXTimes = function(x, theFunction) {
-    for (let i = 0; i < x; i++) {
-        theFunction();
-    }
+exports.callMeXTimes = function (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
 };
