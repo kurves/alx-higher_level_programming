aace476928449c89198f3a60add08a0827e6c5b6
kurves <kavssimply@gmail.com>
2024-04-09 01:52:18 -0700

    fix semi standard coding style

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-    if (base === 10) {
-        return arguments[1];
-    } else {
-        return parseInt(arguments[1]).toString(base);
-    }
+  if (base === 10) {
+    return arguments[1];
+  } else {
+    return parseInt(arguments[1]).toString(base);
+  }
 };
