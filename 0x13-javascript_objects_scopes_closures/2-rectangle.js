a420045bb48fd061b29c002204f7488dc44e7277
kurves <kavssimply@gmail.com>
2024-04-09 13:53:39 -0700

    fix semistandard fixing codingstyle

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 
 class Rectangle {
   constructor (w, h) {
-      if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
       this.width = w;
       this.height = h;
     }
