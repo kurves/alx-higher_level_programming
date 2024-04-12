021eeae176d73954d83096e7efd0f1ff104d1442
kurves <kavssimply@gmail.com>
2024-04-12 11:40:03 -0700

    fix pycodetsyle coding

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -14,7 +14,8 @@ if __name__ == "__main__":
     cur = conn.cursor()
 
     query = "SELECT cities.id, cities.name, states.name FROM cities \
-             JOIN states ON cities.state_id = states.id WHERE states.name = %s \
+             JOIN states ON cities.state_id = states.id \
+             WHERE states.name = %s \
              ORDER BY cities.id"
 
     cur.execute(query, (sys.argv[4],))
