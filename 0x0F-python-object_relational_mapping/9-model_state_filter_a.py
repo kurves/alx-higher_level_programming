7770d0aeca6b21db610f4ea48ea2ef28d4abb70c
kurves <kavssimply@gmail.com>
2024-04-12 11:57:31 -0700

    pycodestyle: fix styling and formatting

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -17,7 +17,8 @@ if __name__ == "__main__":
 
     session = Session()
 
-    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    states = session.query(State).filter(State.name.like('%a%'))
+    .order_by(State.id).all()
 
     for state in states:
         print("{}: {}".format(state.id, state.name))
