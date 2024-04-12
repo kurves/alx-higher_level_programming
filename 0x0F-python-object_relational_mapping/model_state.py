67c143ba1b58a323a39aa7edd952a74faa40add1
kurves <kavssimply@gmail.com>
2024-04-12 11:46:49 -0700

    pycodestyle: fix styling and formating

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """
-Module that contains the class definition of a State and an instance Base = declarative_base()
+Module that contains the class definition of a State
 """
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
 
 class State(Base):
     """
