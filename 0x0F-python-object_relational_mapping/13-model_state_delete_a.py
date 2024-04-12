d3d3ddaebe8d7821d4e18c06dd6b1c9bfee3e292
kurves <kavssimply@gmail.com>
2024-04-12 12:30:24 -0700

    pycodestyle: fix pycodestyle formatting

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -15,10 +15,11 @@ if __name__ == "__main__":
                            pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
-    
+
     session = Session()
 
-    states_to_delete = session.query(State).filter(State.name.like('%a%')).all()
+    states_to_delete = session.query(State).filter
+    (State.name.like('%a%')).all()
 
     for state in states_to_delete:
         session.delete(state)
