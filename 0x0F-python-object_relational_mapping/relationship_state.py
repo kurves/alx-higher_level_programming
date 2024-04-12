aefa30d3eac0964bead97222f2a60a15cb36b072
kurves <kavssimply@gmail.com>
2024-04-12 12:47:36 -0700

    script that creates the State “California” with the City “San Francisco” from the database

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module that contains the class definition of a State.
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from relationship_city import Base, City
+
+
+class State(Base):
+    """
+    Class representing a state in the database.
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
