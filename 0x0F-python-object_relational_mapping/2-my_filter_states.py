2fb5b5b8f38689ed5af5c318dffaa3e89b98c042
kurves <kavssimply@gmail.com>
2024-04-12 11:06:43 -0700

    script that takes in an argument and displays all values

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Script that displays all values in the states table
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=sys.argv[1], passwd=sys.argv[2],
+                           db=sys.argv[3], charset="utf8")
+
+    cur = conn.cursor()
+
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id".format(sys.argv[4])
+    cur.execute(query)
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    conn.close()
