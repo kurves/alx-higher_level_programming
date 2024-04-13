f41777f962a64facd5b453493874cc596f5d86fa
kurves <kavssimply@gmail.com>
2024-04-13 10:33:47 -0700

    prints all City objects from the database

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -18,9 +18,10 @@ if __name__ == "__main__":
 
     session = Session()
 
-    cities = session.query(City).order_by(City.id).all()
+    cities = session.query(City, State).JOIN(State)
 
     for city in cities:
         print("{}: ({}) {}".format(city.state.name, city.id, city.name))
-
+    
+    session.commit()
     session.close()
