2786ba7d2350816e0f7fe2cb3754dfdf1cf4de7c
kurves <kavssimply@gmail.com>
2024-04-13 11:12:10 -0700

    script that lists all City objects from the database

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -3,7 +3,7 @@
 Module that contains the class definition of a State.
 """
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from relationship_city import Base, City
@@ -18,4 +18,4 @@ class State(Base):
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", back_populates="state", cascade="all, delete")
+    cities = relationship("City", backref="state")
