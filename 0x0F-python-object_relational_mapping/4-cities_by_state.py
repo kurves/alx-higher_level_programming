a39eb7c5c0cbb4025af8114eee47cf3a1b03750e
kurves <kavssimply@gmail.com>
2024-04-14 04:35:27 -0700

    ppycodestyle: fix coding stttyle

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
     cur = conn.cursor()
 
     cur.execute("SELECT cities.id, cities.name, states.name \
-                                FROM cities INNER JOIN states ON cities.state_id \
-                                = states.id")
+                FROM cities INNER JOIN states ON cities.state_id \
+                = states.id")
 
     rows = cur.fetchall()
 
