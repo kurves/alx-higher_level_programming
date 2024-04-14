18762a1868a270edaa6733d66069847b48dd039b
kurves <kavssimply@gmail.com>
2024-04-14 04:39:44 -0700

    script that lists all State objects that contain the letter a

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -13,12 +13,13 @@ if __name__ == "__main__":
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
 
+    Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
 
     session = Session()
 
-    states = session.query(State).filter
-    (State.name.like('%a%')).order_by(State.id).all()
+    states = session.query(State).filter(State.name.like('%a%'))
 
     for state in states:
         print("{}: {}".format(state.id, state.name))
