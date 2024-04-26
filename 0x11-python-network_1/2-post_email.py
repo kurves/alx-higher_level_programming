326f5ac196dac26800bf21afaad439cff3b8b5e9
kurves <kavssimply@gmail.com>
2024-04-26 02:50:12 -0700

    pycodestyle: fix styling and formatting

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,7 +8,7 @@ import urllib.parse
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    data = urllib.parse.urlencode({'email':email}).encode()
+    data = urllib.parse.urlencode({'email': email}).encode()
     req = urllib.request.Request(url, data=data, method='POST')
     with urllib.request.urlopen(req) as res:
         body = res.read()
