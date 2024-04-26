7b0c79e0d90c5f92a3b4c3025122b874eee1b3cb
kurves <kavssimply@gmail.com>
2024-04-26 02:22:28 -0700

    script to fetch url using urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,4 +6,7 @@ import urllib.request
 req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
 with urllib.request.urlopen(req) as res:
     data = res.read()
-    print(data)
+    print("Body response:")
+    print("\t- type:", type(data))
+    print("\t- content:", data)
+    print("\t- utf8 content:", data.decode('utf-8'))
