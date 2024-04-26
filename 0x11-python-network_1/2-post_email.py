538e852f885f3b437d8dacba763d64db98b8b761
kurves <kavssimply@gmail.com>
2024-04-26 02:39:24 -0700

    Python script that takes in a URL and an email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""sending data  data using urllibi and returning the body res"""
+
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    email = sys.argv[2]
+    data = urllib.parse.urlencode(email)
+    data = data.encode('ascii')
+    with urllib.request.urlopen(url, data) as req:
+        res = req.read()
+        print(res.decode('utf-8')
