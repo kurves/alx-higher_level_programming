1d2ca26b57a57f8cb12cccdc0cb07cd7c420c330
kurves <kavssimply@gmail.com>
2024-04-26 02:48:22 -0700

    fix compilaton error

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,8 +8,8 @@ import urllib.parse
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    data = urllib.parse.urlencode('email':email).encode()
+    data = urllib.parse.urlencode({'email':email}).encode()
     req = urllib.request.Request(url, data=data, method='POST')
     with urllib.request.urlopen(req) as res:
         body = res.read()
-        print(body.decode('utf-8')
+        print(body.decode('utf-8'))
