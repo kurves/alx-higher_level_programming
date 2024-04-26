f0dc8a643087fd910e9cf1c526436b378088d9d2
kurves <kavssimply@gmail.com>
2024-04-26 02:25:21 -0700

    python script to fetch url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,11 +2,11 @@
 """fetching data using urllib"""
 
 import urllib.request
-
-req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-with urllib.request.urlopen(req) as res:
-    data = res.read()
-    print("Body response:")
-    print("\t- type:", type(data))
-    print("\t- content:", data)
-    print("\t- utf8 content:", data.decode('utf-8'))
+if __name__ == "__main__":
+    req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(req) as res:
+        data = res.read()
+        print("Body response:")
+        print("\t- type:", type(data))
+        print("\t- content:", data)
+        print("\t- utf8 content:", data.decode('utf-8'))
