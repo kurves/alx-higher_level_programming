45c4f801ad1523d093419c1c7efe631521f6646f
kurves <kavssimply@gmail.com>
2024-04-26 03:20:59 -0700

    requesting data using requests

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""fetching data using requests module"""
+
+import requests
+if __name__ == "__main__":
+    req = requests.get("https://alx-intranet.hbtn.io/status")
+        data = req.read()
+        print("Body response:")
+        print("\t- type:", type(data))
+        print("\t- content:", data)
