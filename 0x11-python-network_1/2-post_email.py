201ca50caea17e7d5732d7fca2b409845bdf370a
kurves <kavssimply@gmail.com>
2024-04-26 02:41:35 -0700

    script to send email to url

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,6 +5,7 @@ import urllib.request
 import sys
 
 if __name__ == "__main__":
+    url = sys.argv[1]
     email = sys.argv[2]
     data = urllib.parse.urlencode(email)
     data = data.encode('ascii')
