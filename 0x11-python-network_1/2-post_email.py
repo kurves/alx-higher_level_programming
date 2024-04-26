ae0c6c2ea39f6212d3ca07f2f6a5da2050a3e2b0
kurves <kavssimply@gmail.com>
2024-04-26 02:46:27 -0700

    script tto send email to url

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,12 +3,13 @@
 
 import urllib.request
 import sys
+import urllib.parse
 
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    data = urllib.parse.urlencode(email)
-    data = data.encode('ascii')
-    with urllib.request.urlopen(url, data) as req:
-        res = req.read()
-        print(res.decode('utf-8')
+    data = urllib.parse.urlencode('email':email).encode()
+    req = urllib.request.Request(url, data=data, method='POST')
+    with urllib.request.urlopen(req) as res:
+        body = res.read()
+        print(body.decode('utf-8')
