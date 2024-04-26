8a3be782c92408157425882e7eec9cbf868ebe3d
kurves <kavssimply@gmail.com>
2024-04-26 03:08:51 -0700

    script to handle http error

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""script that displays data in decoded form"""
+
+import urllib.request
+import sys
+from urllib.error import URLError, HTTPError
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as res:
+    except HTTPError as e:
+        print('Error code: ', e.code)
+    except URLError as e:
+        print('Reason: ', e.reason)
+    else:
+        body = res.read()
+        print(body.decode('utf-8'))
