7b2afb91cd7c060bc69539156e72e1be4a348e3c
kurves <kavssimply@gmail.com>
2024-04-26 02:30:46 -0700

    dispalying variable in header response

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""fetching data using urllibi and returning header var"""
+
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as res:
+        request_id = response.headers.get('X-Request-Id')
+        print(request_id)
