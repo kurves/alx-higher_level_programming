863033b16e677f9ed00b8f7e06154109f651c0bc
kurves <kavssimply@gmail.com>
2024-04-26 09:43:07 -0700

    script to get github id

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,16 +2,17 @@
 """script to query github api"""
 import sys
 import requests
+from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
 
     url = "https://api.github.com/user"
-    creds = {"username": username, "password": password}
+    creds =  HTTPBasicAuth(username, password)
     req = requests.post(url, data=creds)
     if req.status_code == 200:
         data = req.json()
         print(data["id"])
     else:
-        print("Failed to retrieve GitHub id. Error code:", response.status_code)
+        print("Failed to retrieve GitHub id. Error code:", req.status_code)
