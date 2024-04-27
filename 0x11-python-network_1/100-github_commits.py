fac9bfe429080dbf0b97f2652defec8d2b4334b9
kurves <kavssimply@gmail.com>
2024-04-27 00:32:10 -0700

    script to query github api

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,15 +5,16 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
-    repo = sys.argv[1]
-    username = sys.argv[2]
-    url = "https://developer.github.com/v3/repos/commits/"
-    creds = HTTPBasicAuth(repo, username)
-    req = requests.get(url, auth=creds)
-    if req.status_code == 200:
-        data = req.json()
-        print(data["commit"])
-    else:
-        print("None")
+    def get_commits(repo, user, count=10):
+        repo = sys.argv[1]
+        username = sys.argv[2]
+        url = "https://developer.github.com/v3/repos/commits/"
+        creds = HTTPBasicAuth(repo, username)
+        req = requests.get(url, auth=creds)
+        if req.status_code == 200:
+            data = req.json()
+            print(data["commit"])
+        else:
+            print("None")
 
 
