d2f5000214be7260f2f90cfa024ac77751b1b0d4
kurves <kavssimply@gmail.com>
2024-04-28 11:43:19 -0700

    pycodestyle: fix style and formatting

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,20 +5,19 @@ import sys
 import requests
 
 if __name__ == "__main__":
-        repo = sys.argv[1]
-        username = sys.argv[2]
-        url = "https://api.github.com/repos/{}/{}/commits".format(username, repo)
-        req = requests.get(url)
-        if req.status_code == 200:
-            data = req.json()
-            try:
-                for i in range(11):
-                    commit = data[i].get("sha")
-                    owner = data[i].get("commit").get("author").get("name")
-                    print("{}: {}".format(commit, owner))
-            except IndexError:
-                pass
-        else:
-            print("None")
-
-
+    repo = sys.argv[1]
+    username = sys.argv[2]
+    url = "https://api.github.com/ \
+    repos/{}/{}/commits".format(username, repo)
+    req = requests.get(url)
+    if req.status_code == 200:
+        data = req.json()
+        try:
+            for i in range(10):
+                commit = data[i].get("sha")
+                owner = data[i].get("commit").get("author").get("name")
+                print("{}: {}".format(commit, owner))
+        except IndexError:
+            pass
+    else:
+        print("None")
