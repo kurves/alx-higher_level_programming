bd51ae02502fb1bc80eded6e722a895a2b7c7af2
kurves <kavssimply@gmail.com>
2024-04-28 11:45:33 -0700

    pycodestyle: fix style and formatting

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,8 +7,7 @@ import requests
 if __name__ == "__main__":
     repo = sys.argv[1]
     username = sys.argv[2]
-    url = "https://api.github.com/ \
-    repos/{}/{}/commits".format(username, repo)
+    url = "https://api.github.com/repos/{}/{}/commits".format(username, repo)
     req = requests.get(url)
     if req.status_code == 200:
         data = req.json()
