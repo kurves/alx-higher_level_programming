cb8c2a6fc593b20eff053a190af3e760a7319669
kurves <kavssimply@gmail.com>
2024-05-21 10:13:59 -0700

    script to list tabls

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,15 +1,6 @@
-#!/bin/bash
+-- script to list tables
+-- list table in database
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <database_name>"
-    exit 1
-fi
-
-database_name="$1"
-
-
-read -p "Enter MySQL username: " username
-read -sp "Enter MySQL password: " password
-echo
-
-mysql -u "$username" -p"$password" -e "USE $database_name; SHOW TABLES;"
+SELECT TABLE_NAME
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA = 'mysql';
