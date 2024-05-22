44729e17b8625114ee5f8efa8dc0d2290a2c1636
kurves <kavssimply@gmail.com>
2024-05-22 09:18:44 -0700

    make file executable

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,20 +2,16 @@
 
 const fs = require('fs');
 
-// Get the file path and the string to write from the command line arguments
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-// Check if both arguments are provided
 if (!filePath || !content) {
-  console.error('Please provide both a file path and a string to write.');
+  console.error('Please provide file path and a string.');
   process.exit(1);
 }
 
-// Write the content to the file in utf-8 encoding
 fs.writeFile(filePath, content, 'utf8', (err) => {
   if (err) {
-    // Print the error object if an error occurred
     console.error('Error writing to the file:', err);
     return;
   }
