ab1db992995568cb1ffd60fbb7d7da4395841d2c
kurves <kavssimply@gmail.com>
2024-05-21 23:53:39 -0700

    script to read file contents

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+if (!filePath) {
+  console.error('Please provide a file path as the first argument.');
+  process.exit(1);
+}
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading the file:', err);
+    return;
+  }
+  console.log(data);
+});
