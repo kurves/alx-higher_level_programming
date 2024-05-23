b7663350da657d8ade34b2f614523d03afc57f46
kurves <kavssimply@gmail.com>
2024-05-23 06:30:31 -0700

    fix styling and formatting

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -9,22 +9,18 @@ request(apiUrl, function (err, response, body) {
     console.error(err);
     return;
   }
-  
   if (response.statusCode !== 200) {
     console.error('An error occurred. Status code:', response.statusCode);
     return;
   }
-  
   const filmData = JSON.parse(body);
   const characters = filmData.characters;
-  
   characters.forEach(characterUrl => {
     request(characterUrl, function (err, response, body) {
       if (err) {
         console.error(err);
         return;
       }
-      
       if (response.statusCode === 200) {
         const characterData = JSON.parse(body);
         console.log(characterData.name);
@@ -34,4 +30,3 @@ request(apiUrl, function (err, response, body) {
     });
   });
 });
-
