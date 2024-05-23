4bfdd3470cbd744d3b5b8fc32aeaa671dc3652bb
kurves <kavssimply@gmail.com>
2024-05-23 04:28:18 -0700

    script to write cióntents to file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -20,8 +20,6 @@ request(url, (error, response, body) => {
   fs.writeFile(filePath, body, 'utf8', (err) => {
     if (err) {
       console.error('Error writing to the file:', err);
-      return;
     }
-    console.log(`Content written to ${filePath} successfully.`);
   });
 });
