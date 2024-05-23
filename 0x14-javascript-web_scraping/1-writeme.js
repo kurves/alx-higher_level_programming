7f1d10b29af310ada7afabcfe9022e455ecda16b
kurves <kavssimply@gmail.com>
2024-05-23 04:13:34 -0700

    SCRIPT TO WRITE CINTENTS TO FILE

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,11 +5,6 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-if (!filePath || !content) {
-  console.error('Please provide file path and a string.');
-  process.exit(1);
-}
-
 fs.writeFile(filePath, content, 'utf8', (err) => {
   if (err) {
     console.error('Error writing to the file:', err);
