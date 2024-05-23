123e61665a1274f453622187633447c7d4d0e330
kurves <kavssimply@gmail.com>
2024-05-23 04:25:07 -0700

    script to count movie titles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,31 +1,14 @@
 #!/usr/bin/node
 
 const request = require('request');
-
 const apiUrl = process.argv[2];
-
-if (!apiUrl) {
-  console.error('Please provide the API URL as the first argument.');
-  process.exit(1);
-}
-
-const characterId = 18;
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error making the request:', error);
-    return;
+request(apiUrl, function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
-
-  const data = JSON.parse(body);
-
-  let count = 0;
-
-  data.results.forEach(film => {
-    if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
-      count++;
-    }
-  });
-
-  console.log(count);
 });
