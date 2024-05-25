4dab9ec7f2b18bed2251a2a59bc2ba4ecd4e4f36
kurves <kavssimply@gmail.com>
2024-05-25 12:31:55 -0700

    script to insert values

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,7 +1,4 @@
-#!/bin/bash
-
 -- script to insert row
 -- insert name and id
-database_name=$1
 
-mysql -u root -p -e "USE $database_name; INSERT INTO first_table (id, name) VALUES (89, 'Best School');"
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
