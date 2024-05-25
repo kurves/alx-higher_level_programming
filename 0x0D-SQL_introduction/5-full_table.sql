c1aa21090de2d913318c6a4f64a861f2a70c8940
kurves <kavssimply@gmail.com>
2024-05-25 12:22:58 -0700

    script to show table description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,7 +1,4 @@
-
+-- script to show table description
 -- script to describe database
 
-database_name=$1
-table_name=$2
-
-mysql -u root -p -e "USE $database_name; SHOW COLUMNS FROM $table_name;"
+SHOW CREATE TABLE `first_table`;
