d07b09aa33e8cca26c3f2978a15f330ebc6562b6
kurves <kavssimply@gmail.com>
2024-05-25 12:26:02 -0700

    script to list table info

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,6 +1,3 @@
-
+-- script to list table
 -- script to list values from tale
-
-database_name=$1
-
-mysql -u root -p -e "USE $database_name; SELECT * FROM first_table;"
+SELECT * FROM `first_table`;
