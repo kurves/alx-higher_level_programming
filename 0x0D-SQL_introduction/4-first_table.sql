46318446ee6b45c7cabc99f02bb7450cc72b159a
kurves <kavssimply@gmail.com>
2024-05-25 12:16:09 -0700

    script to create table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -3,10 +3,8 @@
 
 database_name="$1"
 
-
-sql_query="CREATE TABLE IF NOT EXISTS first_table (
+CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
-);"
+);
 
-mysql -u your_username -p your_password -e "USE $database_name; $sql_query"
