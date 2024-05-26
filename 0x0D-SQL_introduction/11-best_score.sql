97096817391ea236484131c72138f28a49670645
kurves <kavssimply@gmail.com>
2024-05-26 00:32:37 -0700

    script to list top values

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,13 +1,5 @@
-#!/bin/bash
-
 -- script to record all scores
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <database_name>"
-    exit 1
-fi
-
-DB_NAME="$1"
 
 SQL_QUERY="SELECT score, name FROM ${DB_NAME}.second_table WHERE score >= 10 ORDER BY score DESC;"
 
