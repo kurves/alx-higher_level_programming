4337c150d7a6210e4cbb0f31e8163e0a817bc0fb
kurves <kavssimply@gmail.com>
2024-05-26 00:37:29 -0700

    script to update values

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,6 +1,4 @@
 --script to records list
 
 
-SQL_QUERY="UPDATE ${DB_NAME}.second_table SET score = 10 WHERE name = 'Bob';"
-
-mysql -e "$SQL_QUERY"
+UPDATE second_table SET score = 10 WHERE name = 'Bob';"
