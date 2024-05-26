7be09cd479fb558a6bdec9c30782c6e477298c8f
kurves <kavssimply@gmail.com>
2024-05-26 00:36:32 -0700

    script to update rows

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,13 +1,5 @@
-#!/bin/bash
-
 --script to records list
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <database_name>"
-    exit 1
-fi
-
-DB_NAME="$1"
 
 SQL_QUERY="UPDATE ${DB_NAME}.second_table SET score = 10 WHERE name = 'Bob';"
 
