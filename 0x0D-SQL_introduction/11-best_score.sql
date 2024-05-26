420638a1bf801c56257c5269832f95861797bb3e
kurves <kavssimply@gmail.com>
2024-05-26 00:33:44 -0700

    script to list top values

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,6 +1,3 @@
 -- script to record all scores
 
-
-SQL_QUERY="SELECT score, name FROM ${DB_NAME}.second_table WHERE score >= 10 ORDER BY score DESC;"
-
-mysql -e "$SQL_QUERY"
+SELECT score, name FROM ${DB_NAME}.second_table WHERE score >= 10 ORDER BY score DESC;
