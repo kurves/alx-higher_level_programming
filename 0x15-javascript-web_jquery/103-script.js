b181760151be23705be0937cf237c8d2f6a6c6f7
kurves <kavssimply@gmail.com>
2024-05-30 11:34:10 -0700

    script to fetch language api

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
-    function fetchHello() {
-        const langCode = $('#language_code').val();
-        if (langCode) {
-            $.getJSON(`https://www.fourtonfish.com/hellosalut/hello/?lang=${langCode}`, function(data) {
-                $('#hello').text(data.hello);
-            }).fail(function() {
-                $('#hello').text('Translation not available.');
-            });
-        } else {
-            $('#hello').text('Please enter a language code.');
-        }
-    }
+  function fetchHello() {
+      const langCode = $('#language_code').val();
+      if (langCode) {
+          $.getJSON(`https://hellosalut.stefanbohacek.dev/?lang=${langCode}`, function(data) {
+              $('#hello').text(data.hello);
+          }).fail(function() {
+              $('#hello').text('Translation not available.');
+          });
+      } else {
+          $('#hello').text('Please enter a language code.');
+      }
+  }
 
-    $('#btn_translate').click(fetchHello);
+  $('#btn_translate').click(fetchHello);
 
-    $('#language_code').keypress(function(event) {
-        if (event.which === 13) {
-            fetchHello();
-        }
-    });
+  $('#language_code').keypress(function(event) {
+      if (event.which === 13) {
+          fetchHello();
+      }
+  });
 });
