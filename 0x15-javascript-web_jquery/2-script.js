e59a31bd6c24d9103cd44531f15a5571a567d6fd
kurves <kavssimply@gmail.com>
2024-05-30 12:05:13 -0700

    semistandard: fix formattig and stylling

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,5 +1,7 @@
-$(document).ready(function() {
-    $('DIV#red_header').click(function() {
-        $('header').css('color', '#FF0000');
-    });
+/* global $ */
+
+$(document).ready(function () {
+  $('DIV#red_header').click(function () {
+    $('header').css('color', '#FF0000');
+  });
 });
