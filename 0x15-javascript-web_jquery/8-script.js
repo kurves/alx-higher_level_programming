1ea63f6ab7b24f130c5128177bea3869cd14b466
kurves <kavssimply@gmail.com>
2024-05-30 12:12:27 -0700

    semistandard: fix styling and formatting

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,7 +1,9 @@
-$(document).ready(function() {
-    $.get("https://swapi-api.alx-tools.com/api/films/?format=json", function(data) {
-        data.results.forEach(function(movie) {
-            $('#list_movies').append('<li>' + movie.title + '</li>');
-        });
+/* global $ */
+
+$(document).ready(function () {
+  $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+    data.results.forEach(function (movie) {
+      $('#list_movies').append('<li>' + movie.title + '</li>');
     });
+  });
 });
