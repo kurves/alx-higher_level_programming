f91f95b715b11ec74fae576f0c43b7baea2a89a9
kurves <kavssimply@gmail.com>
2024-06-01 04:25:56 -0700

    script to list scores grater tha 10

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,6 @@
 -- script to record all scores
 
-SELECT score, name FROM ${DB_NAME}.second_table WHERE score >= 10 ORDER BY score DESC;
+SELECT score, name
+FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
