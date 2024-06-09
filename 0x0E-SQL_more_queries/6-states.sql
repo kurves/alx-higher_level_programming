2a6a07961d4ba8c311a507b9a3a6d9449da61132
kurves <kavssimply@gmail.com>
2024-06-09 04:57:48 -0700

    script to create table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,9 @@
 -- scriot to add states to table
 
-CREATE TABLE IF NOT EXISTS ${TABLE_NAME} (
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states (
     id INT AUTO_INCREMENT UNIQUE NOT NULL,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
