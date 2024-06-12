22593e0724ab01eb1d9d867afd7b9677e5965df4
kurves <kavssimply@gmail.com>
2024-06-12 12:34:18 -0700

    script to join tables

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -5,3 +5,5 @@ SELECT cities.id, cities.name, (
 ) AS state_name
 FROM cities
 ORDER BY cities.id ASC;
+
+SELECT cities.id, cities.name, states.name AS state_name FROM states JOIN cities.id  ON cities.state_id = states.id ORDER BY cities.id ASC;
