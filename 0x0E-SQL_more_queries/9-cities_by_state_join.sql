314296824750002114b85a5f3bf53e616bf35f53
kurves <kavssimply@gmail.com>
2024-06-12 12:27:47 -0700

    cities by state

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- script to create cities by states
+
+SELECT cities.id, cities.name, (
+    SELECT states.name FROM states WHERE states.id = cities.state_id
+) AS state_name
+FROM cities
+ORDER BY cities.id ASC;
