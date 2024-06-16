52d8611af1a8f0ad21aa4f28f5f5e2c565fa93c8
kurves <kavssimply@gmail.com>
2024-06-16 08:54:11 -0700

    script to join tables

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,4 +3,5 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
   FROM tv_shows
   JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+  JOIN $DB_NAME.genres g ON tg.genre_id = g.id
   ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
