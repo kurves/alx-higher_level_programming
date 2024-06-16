41cc7421c1e02d890bebfd0110d2525ea80daed0
kurves <kavssimply@gmail.com>
2024-06-16 08:56:56 -0700

    scirpt to join tables

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,5 +3,6 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
   FROM tv_shows
   JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-  JOIN $DB_NAME.genres g ON tg.genre_id = g.id
+  JOIN tv_show_genres  ON tv_show_genres.genre_id = genre.id
+  GROUP BY tv_shows.title, tv_show_genres.genre_id
   ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
