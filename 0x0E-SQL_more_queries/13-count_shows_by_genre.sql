99a878b4dced7900ddff0a56ab5713f219eb3ef6
kurves <kavssimply@gmail.com>
2024-06-28 11:59:10 -0700

    script to join tables

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- script to join tables
 
-SELECT tv_enres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
-FROM genres
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
 JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
-GROUP BY genres.name
+GROUP BY genres
 HAVING COUNT(tv_show_genres.show_id) > 0
 ORDER BY number_of_shows DESC;
