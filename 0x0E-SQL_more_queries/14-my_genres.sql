769ad8e5b60682dbc7e9ebc3f77ed795346491e6
kurves <kavssimply@gmail.com>
2024-07-15 08:52:40 -0700

    script to join tables

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -7,7 +7,3 @@ JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name ASC;
-
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_genres.name ASC;
