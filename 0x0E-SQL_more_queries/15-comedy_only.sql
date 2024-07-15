a3d0de147fe0e7b2d856d0052f8bddbd2f8de277
kurves <kavssimply@gmail.com>
2024-07-15 08:57:47 -0700

    scipt to select tv shows

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -2,12 +2,7 @@
 SELECT tv_shows.title
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-SELECT
-    g.name AS tv_genres_name
-FROM
-    tv_shows t
-JOIN
-    tv_show_genres tg ON t.id = tg.tv_show_id
+
 JOIN
     genres g ON tg.genre_id = g.id
 WHERE
