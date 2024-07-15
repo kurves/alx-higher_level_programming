959504baa5c93d2d73f13cd47befa354a9ea1ebb
kurves <kavssimply@gmail.com>
2024-07-15 08:48:42 -0700

    script to join tables

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,9 @@
 -- script to join tables
 
+
+SELECT tv_genres.name AS genre
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 SELECT tv_genres.name
 FROM  tv_shows
 JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
