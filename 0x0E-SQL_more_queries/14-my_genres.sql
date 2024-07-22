78886d73648f674d5c27c13a1b01917ed2d84012
kurves <kavssimply@gmail.com>
2024-07-22 09:39:43 -0700

    script to list shows and genre

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -7,16 +7,3 @@ JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name ASC;
 
-
-SELECT
-    tv_shows.title,
-    tv_genres.name
-FROM
-    tv_shows
-LEFT JOIN
-    tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN
-    tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY
-    tv_shows.title ASC,
-    tv_genres.name ASC;
