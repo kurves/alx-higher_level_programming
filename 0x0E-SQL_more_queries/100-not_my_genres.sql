2efbe039da92a77a972ba06ea5b5c6c43b019b4e
kurves <kavssimply@gmail.com>
2024-07-23 12:28:08 -0700

    script to list shows

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,3 +1,14 @@
+--  show titled "Dexter"
+
 SELECT @dexter_show_id := id
 FROM tv_shows
 WHERE title = 'Dexter';
+
+SELECT tv_genres.name
+FROM tv_genres
+WHERE tv_genres.id NOT IN (
+    SELECT tv_show_genres.genre_id
+    FROM tv_show_genres
+    WHERE tv_show_genres.show_id = @dexter_show_id
+)
+ORDER BY tv_genres.name ASC;
