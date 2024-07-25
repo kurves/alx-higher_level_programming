9a69bf02a946436ad9455f3cab18a10227831155
kurves <kavssimply@gmail.com>
2024-07-25 12:32:02 -0700

    scrio[t to find show

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -7,3 +7,8 @@ WHERE name = 'Comedy';
 SELECT tv_shows.title
 FROM tv_shows
 WHERE tv_shows.id NOT IN (
+	SELECT tv_show_genres.show_id
+    FROM tv_show_genres
+    WHERE tv_show_genres.genre_id = @comedy_genre_id
+)
+ORDER BY tv_shows.title ASC;
